478f0f483b771b6bc6ff9d82250e2fd61a2b4fec
christopherross <christopherross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-27 14:46:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stocktwits-ticker-auto-tagger/trunk@337835 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== WP Auto Tagger ===
+Contributors: christopherross
+Donate link:  http://thisismyurl.com/stocktwits-auto-tagger/
+Tags: auto, suggest, tags, post, tagging, keywords, stocktwits
+Requires at least: 2.5
+Tested up to: 3.0.4
+Stable tag: 1.0.0
+
+An auto tagger plugin designed to automatically include valid stock ticker symbols as tags, along with their official name.
+
+
+== Description ==
+
+An auto tagger plugin designed to automatically include valid stock ticker symbols as tags, along with their official name.
+
+The plugin works by detecting $ticker symbols such as $appl or $gm and converting it to a tag, along with an automated process to fetch the stock name from Yahoo! Finance and include the company name as an additional tag for search engine optimization of your financial blog.
+
+
+== Installation ==
+
+
+== Frequently Asked Questions ==
+
+= How does this work? =
+
+When you make a post in WordPress, simply indicate that you're posting a stock ticker using the dollar sign symbol. For example, $gm to indicate you're posting about General Motors Common and the plugin will automatically add gm as well as General Motors Common to your post tags.
+
+= Can I automatically link the tag? =
+
+We have another plugin called the StockTwits Ticker Link (http://wordpress.org/extend/plugins/stocktwits-ticker-links/) that will automatically link your $tickers to social media streams for the ticker.
+
+== Screenshots ==
+
---
+++ stocktwits-auto-tagger.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: StockTwits Auto Tagger
+Plugin URI: http://thisismyurl.com/stocktwits-auto-tagger/
+Description: Automatically find stock symboltags based on content of the blog post.
+Version: 1.0.0
+Author: christopherross
+Author URI: http://stocktwits.com
+*/
+
+add_action('wp_insert_post', 'stocktwits_auto_stocktwits_gettags', 10, 2);
+function stocktwits_auto_stocktwits_gettags($post_id, $post) {
+	$tags=$post->tags_input;
+	if(is_array($tags)) $tags=implode(',',$tags);
+	if(empty($tags) && !empty($_POST['tags_input'])) $tags=$_POST['tags_input'];
+	$tags=stocktwits_gettags($post->post_title,$post->post_content,$tags);
+	if(!is_array($tags)) return;
+	wp_add_post_tags($post_id,$tags);
+
+}
+
+function stocktwits_gettags($title,$content,$tags) {
+	
+	
+	$symbols = explode("$",strip_tags($content));
+	
+	foreach ($symbols as $symbol) {
+		$include = false;
+		$symbol = str_replace(","," ",$symbol);
+		$temp = explode(" ",$symbol);
+		$symbol = trim($temp[0]);
+		if (strlen($symbol) >0 ) {
+			if (substr($content, 0, 1) == "$" && $count==0) {
+				$include = true;	
+			} elseif ($count > 0) {
+				$include = true;
+			}
+			
+			
+			$exclude = "1,2,3,4,5,6,7,8,9,0";
+			$exclude = explode(",",$exclude);
+			foreach ($exclude as $excludeitem) {
+				if (substr_count($temp[0],$excludeitem)> 0) {
+					$include = false;
+				}
+			}
+			
+			if ($include) {
+				$symbollist[] = $symbol;
+			}
+		}
+	
+		$count++;
+	}
+	
+	if (is_array($symbollist)) {	
+		$symbollist = array_unique($symbollist);
+		
+		foreach ($symbollist as $symbol) {
+			
+			if(function_exists('curl_init')) {
+						
+				$ch = curl_init();
+				$url = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query='.$symbol.'&callback=YAHOO.Finance.SymbolSuggest.ssCallback';
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+				$response = curl_exec($ch);
+				if(curl_errno($ch)) return curl_error($ch);
+				curl_close($ch);
+				$results=unserialize($response);
+			
+			}	 else {
+				$response = file_get_contents($url);
+			}
+			
+				$yahoostock=($response);
+				$yahoostock=explode('"name": "',$yahoostock);
+				$yahoostock=explode('","',$yahoostock[1]);
+				unset($stock);
+				$stock = trim($yahoostock[0]);
+				
+				if ($stock != "") {
+					$tags[] = $stock;
+				}
+				$tags[] = $symbol;
+		}
+	}
+	
+	if (is_array($tags)) {
+		$tags = array_unique($tags);
+	}
+	return $tags;
+}
